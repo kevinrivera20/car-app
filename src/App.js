813eb5b0042299aa5c0f1c74221aa8eb324d14fa
Kevin Rivera <kevinsantiagoriverarestrepo@gmail.com>
2023-02-10 14:01:10 -0500

    updates principales

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {  useRef ,useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+
+  const obtResult = useRef(null);
+
+
+  const [selectedCar, setSelectedCar] = useState("");
+
+  const [priceCar, setPriceCar] = useState("");
+
+
+ 
+
+  const handleClick = (e) => {
+    setSelectedCar(obtResult.current.value);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h3>Seleccione el vehiculo</h3>
+      <select 
+        class="form-select" 
+        aria-label="Default select example" 
+        ref={obtResult}
         >
-          Learn React
-        </a>
-      </header>
+        <option selected disabled>Vehiculo</option>
+        <option value="Ford Fiesta">Ford Fiesta</option>
+        <option value="Ford Focus">Ford Focus</option>
+      </select>
+      <br />
+      <button type="button" class="btn btn-primary" onClick={handleClick}>Calcular</button>
+      <br />  
+      <br />
+      <h1>Resultado: {selectedCar}</h1>
+      <br />
+      <h2>Valor del Vehiculo: {priceCar}</h2>
     </div>
   );
 }
